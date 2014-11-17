12df8b55daf2648befa8e8856139bf09d1d633bf
flipmcf <flipmcf@gmail.com>
2014-11-17 15:24:38 -0500

    better tests for youtube iframe replacements - based on feedback on BBGMN-1183

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,18 @@
 import unittest
 
-from ..utils import replaceEmbedsWithIframes
 from kss.core.BeautifulSoup import BeautifulSoup
+import urlparse
+
+from ..utils import replaceEmbedsWithIframes
 
 #Run with ./bin/test Products.rfasite.mobapp
 
 class ReplaceEmbedsTests(unittest.TestCase):
-    
-    def test1(self):
+        
+
+    def testNoHeight(self):
+        """resulting iframe output should not contain height parameter"""
+        
         inputString = """<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
         codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"
             height="360" width="640">
@@ -20,54 +25,81 @@ class ReplaceEmbedsTests(unittest.TestCase):
     </object>
             """
         
-        expectedOutput = """<iframe src="//www.youtube.com/embed/kc658mQqoU0" width="420" frameborder="0" height="315"></iframe>
-        """
+        
         inputSoup = BeautifulSoup(inputString)
-        expectSoup = BeautifulSoup(expectedOutput)
         
-        result = replaceEmbedsWithIframes(inputSoup)
+        resultSoup = replaceEmbedsWithIframes(inputSoup)
+        iframeElem = resultSoup.find('iframe')
         
-        self.assertEqual(str(result), str(expectSoup))
+        self.assertIsNone(iframeElem.get('height'), "Height was defined - should be None")
         
-        
-    
-    def test2(self):
-        inputString = """<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"
- height="360" width="630">
-<param name="allowFullScreen" value="true" />
-<param name="allowscriptaccess" value="always" />
-<param name="src" value="http://www.youtube.com/v/rdOAgdQoGxM&version=3" />
-<param name="allowfullscreen" value="true" /><embed height="360" width="630" src="http://www.youtube.com/v/rdOAgdQoGxM&version=3" allowscriptaccess="always" allowfullscreen="true" type="application/x-shockwave-flash"></embed>
-</object>
-
+    def testWidth100(self):
+        """resulting iframe output should have width set to %100"""
+        inputString = """<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+        codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"
+            height="360" width="640">
+      <param name="allowFullScreen" value="true" />
+      <param name="allowscriptaccess" value="always" />
+      <param name="src" value="https://www.youtube.com/v/kc658mQqoU0" />
+      <param name="allowfullscreen" value="true" />
+      <embed height="360" width="640" src="https://www.youtube.com/v/kc658mQqoU0" allowscriptaccess="always" allowfullscreen="true" type="application/x-shockwave-flash">
+      </embed>
+    </object>
             """
         
-        expectedOutput = """<iframe src="//www.youtube.com/embed/rdOAgdQoGxM" width="420" frameborder="0" height="315"></iframe>
-        """
-        inputSoup = BeautifulSoup(inputString)
-        expectSoup = BeautifulSoup(expectedOutput)
-        
-        result = replaceEmbedsWithIframes(inputSoup)
-        
-        self.assertEqual(str(result), str(expectSoup))
-        
-        
-    def test3(self):
-        inputString = """<object height="315" width="420" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000">
-<param value="true" name="allowFullScreen">
-<param value="always" name="allowscriptaccess">
-<param value="https://www.youtube.com/v/Rh4v5wIC7_s?version=3&amp;hl=en_US" name="src">
-<param value="true" name="allowfullscreen"><embed height="315" width="420" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" src="https://www.youtube.com/v/Rh4v5wIC7_s?version=3&amp;hl=en_US">
-</object>"""
-        expectedOutput = """<iframe src="//www.youtube.com/embed/Rh4v5wIC7_s" width="420" frameborder="0" height="315"></iframe>"""
         
         inputSoup = BeautifulSoup(inputString)
-        expectSoup = BeautifulSoup(expectedOutput)
         
-        result = replaceEmbedsWithIframes(inputSoup)
+        resultSoup = replaceEmbedsWithIframes(inputSoup)
+        iframeElem = resultSoup.find('iframe')
         
-        self.assertEqual(str(result), str(expectSoup))
+        self.assertEqual('%100', iframeElem.get('width'))
         
+    
+    def testNoAllowFullscreen(self):
+        """resulting iframe output should have width set to %100"""
+        inputString = """<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+        codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"
+            height="360" width="640">
+      <param name="allowFullScreen" value="true" />
+      <param name="allowscriptaccess" value="always" />
+      <param name="src" value="https://www.youtube.com/v/kc658mQqoU0" />
+      <param name="allowfullscreen" value="true" />
+      <embed height="360" width="640" src="https://www.youtube.com/v/kc658mQqoU0" allowscriptaccess="always" allowfullscreen="true" type="application/x-shockwave-flash">
+      </embed>
+    </object>
+            """
+        
+        
+        inputSoup = BeautifulSoup(inputString)
+        
+        resultSoup = replaceEmbedsWithIframes(inputSoup)
+        iframeElem = resultSoup.find('iframe')
+        self.assertIsNone(iframeElem.get('allowfullscreen'))
+    
+    def testIframeSrcNoParams(self):
+        """resulting iframe src should not contain any parameters"""
+        inputString = """<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+        codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"
+            height="360" width="640">
+      <param name="allowFullScreen" value="true" />
+      <param name="allowscriptaccess" value="always" />
+      <param name="src" value="https://www.youtube.com/v/QhYO_KwL2P0?hl=en_GB&amp;version=3&amp;rel=0&amp;controls=0&amp;showinfo=0" />
+      <param name="allowfullscreen" value="true" />
+      <embed height="360" width="640" src="https://www.youtube.com/v/kc658mQqoU0" allowscriptaccess="always" allowfullscreen="true" type="application/x-shockwave-flash">
+      </embed>
+    </object>
+            """
+        inputSoup = BeautifulSoup(inputString)
+        
+        resultSoup = replaceEmbedsWithIframes(inputSoup)
+        iframeElem = resultSoup.find('iframe')
+        src = iframeElem.get('src')
+
+        parsed = urlparse.urlparse(src)
+        
+        self.assertEqual('', parsed.query)
+                
 def test_suite():
     return unittest.TestSuite( 
         (unittest.makeSuite(ReplaceEmbedsTests),
