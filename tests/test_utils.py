992c39a71f827d668c6b4794a7acca3184756bb5
flipmcf <flipmcf@gmail.com>
2014-12-10 13:54:02 -0500

    More work on filtering out embed codes from story body | vietnamese audio #319 | Image scaling BBGMN-1227

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,13 +3,136 @@ import unittest
 from kss.core.BeautifulSoup import BeautifulSoup
 import urlparse
 
+from ..utils import cleanHtml
 from ..utils import replaceEmbedsWithIframes
+from ..utils import removeStylesFromInlineImages
+from ..utils import removeWidthHeightFromInlineImages
+
+import re
 
 #Run with ./bin/test Products.rfasite.mobapp
+
+class CleanHtmlTests(unittest.TestCase):
+    
+    def testReturnsSane(self):
+        inputString = "<div> I don't care </div>"
+        result = cleanHtml(inputString)
+        self.assertEqual(inputString, result)
+
+class InlineImageTests(unittest.TestCase):
+    """resulting html of body that contains inline captioned images"""
+    
+    def testNoInlineStyles(self):
+        inputString = """<p>
+  <div style="width:620px;" class="image-inline captioned"> 
+   <div style="width:620px;"> 
+     <img src="http://www.rfa.org/cantonese/news/HK-clearance-12082014074647.html/1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          alt="1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          title="1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          height="613" width="620" /> 
+   </div> 
+   <div class="image-caption" style="width:620px;"> 
+      caption text here
+   </div> 
+  </div>
+</p> 
+And Again!
+<p>
+  <div style="width:620px;" class="image-inline captioned"> 
+   <div style="width:620px;"> 
+     <img src="http://www.rfa.org/cantonese/news/HK-clearance-12082014074647.html/1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          alt="1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          title="1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          height="613" width="620" /> 
+   </div> 
+   <div class="image-caption" style="width:620px;"> 
+      caption text here
+   </div> 
+  </div>
+</p>
+"""
+        
+        inputSoup = BeautifulSoup(inputString)
+        
+        resultSoup = removeStylesFromInlineImages(inputSoup)
+        
+        divElemList = resultSoup.findAll('div', attrs={'class': lambda x: x and re.search('image-inline(\s|$)', x)})
+        
+        def theTest(elem):
+            if isinstance(elem, basestring):
+                return
+            self.assertIsNone(elem.get('style'))
+            for elem in elem.contents:
+                theTest(elem)
+        
+        for elem in divElemList:
+            theTest(elem)
+            
+    def testNoImageSizeSpecified(self):
+        inputString = """<p>
+  <div style="width:620px;" class="image-inline captioned"> 
+   <div style="width:620px;"> 
+     <img src="http://www.rfa.org/cantonese/news/HK-clearance-12082014074647.html/1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          alt="1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          title="1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          height="613" width="620" /> 
+   </div> 
+   <div class="image-caption" style="width:620px;"> 
+      caption text here
+   </div> 
+  </div>
+</p> 
+And Again!
+<p>
+  <div style="width:620px;" class="image-inline captioned"> 
+   <div style="width:620px;"> 
+     <img src="http://www.rfa.org/cantonese/news/HK-clearance-12082014074647.html/1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          alt="1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          title="1208HK-CLEARANCE-Benny-Tai620.jpg" 
+          height="613" width="620" /> 
+   </div> 
+   <div class="image-caption" style="width:620px;"> 
+      caption text here
+   </div> 
+  </div>
+</p>
+"""
+        inputSoup = BeautifulSoup(inputString)
+        
+        resultSoup = removeWidthHeightFromInlineImages(inputSoup)
+        
+        images = resultSoup.findAll('img')
+        
+        for img in images:
+            self.assertIsNone(img.get('height'))
+            self.assertIsNone(img.get('width'))
+        
+        
 
 class ReplaceEmbedsTests(unittest.TestCase):
         
 
+    def testNotYoutube(self):
+        """This should do nothing"""
+        
+        inputString="""<p>
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" height="25" id="audioplayer1" width="240">
+<param name="data" value="http://www.rfa.org/vietnamese/manuallyupload/audio-player/player.swf" />
+<param name="FlashVars" value="playerID=1&amp;bg=0x00FFFF&amp;leftbg=0x3366FF&amp;lefticon=0xFFFFFF&amp;rightbg=0xFF6633&amp;rightbghover=0x999999&amp;righticon=0x14FF14&amp;righticonhover=0xffffff&amp;text=0x666666&amp;slider=0x666666&amp;track=0xFFFFFF&amp;border=0x666666&amp;loader=0x9FFFB8&amp;soundFile=http://www.rfa.org/vietnamese/in_depth/role-of-vn-nati-assem-questioned-12032013124357.html/12032013-hienphap-tq.mp3" />
+<param name="quality" value="high" />
+<param name="menu" value="false" />
+<param name="bgcolor" value="FFFFFF" />
+<param name="src" value="http://www.rfa.org/vietnamese/manuallyupload/audio-player/player.swf" />
+<param name="flashvars" value="playerID=1&amp;bg=0x00FFFF&amp;leftbg=0x3366FF&amp;lefticon=0xFFFFFF&amp;rightbg=0xFF6633&amp;rightbghover=0x999999&amp;righticon=0x14FF14&amp;righticonhover=0xffffff&amp;text=0x666666&amp;slider=0x666666&amp;track=0xFFFFFF&amp;border=0x666666&amp;loader=0x9FFFB8&amp;soundFile=http://www.rfa.org/vietnamese/in_depth/role-of-vn-nati-assem-questioned-12032013124357.html/12032013-hienphap-tq.mp3" /><embed height="25" width="240" src="http://www.rfa.org/vietnamese/manuallyupload/audio-player/player.swf" bgcolor="FFFFFF" menu="false" quality="high" flashvars="playerID=1&amp;bg=0x00FFFF&amp;leftbg=0x3366FF&amp;lefticon=0xFFFFFF&amp;rightbg=0xFF6633&amp;rightbghover=0x999999&amp;righticon=0x14FF14&amp;righticonhover=0xffffff&amp;text=0x666666&amp;slider=0x666666&amp;track=0xFFFFFF&amp;border=0x666666&amp;loader=0x9FFFB8&amp;soundFile=http://www.rfa.org/vietnamese/in_depth/role-of-vn-nati-assem-questioned-12032013124357.html/12032013-hienphap-tq.mp3" data="http://www.rfa.org/vietnamese/manuallyupload/audio-player/player.swf" id="audioplayer1" type="application/x-shockwave-flash"></embed>
+</object>
+</p>"""
+        
+        inputSoup = BeautifulSoup(inputString)
+        
+        resultSoup = replaceEmbedsWithIframes(inputSoup)
+        
+        self.assertEqual(str(inputSoup), str(resultSoup))
+        
     def testNoHeight(self):
         """resulting iframe output should not contain height parameter"""
         
@@ -142,5 +265,7 @@ class ReplaceEmbedsTests(unittest.TestCase):
 def test_suite():
     return unittest.TestSuite( 
         (unittest.makeSuite(ReplaceEmbedsTests),
+         unittest.makeSuite(InlineImageTests),
+         unittest.makeSuite(CleanHtmlTests),
          )  
     )
